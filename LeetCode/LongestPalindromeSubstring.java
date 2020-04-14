e82c054a0098efe96c7ca90f774d23c6bf2ee1c0
Morshed <morshedalamustc22@gmail.com>
2020-04-15 03:18:18 +0600

    LongestPalindromeSubstring problem second approach has been submitted .Time complexity O(n^2) and Space complexity O(1).

--- LeetCode/LongestPalindromeSubstring.java
+++ LeetCode/LongestPalindromeSubstring.java
@@ -1,12 +1,14 @@
 public class LongestPalindromeSubstring {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        System.out.println(longestPalindrome(""));
+        //System.out.println(longestPalindrome("aaaabbaa"));
+        System.out.println(longestPalSubstr("aaaabbaa"));
         long end = System.currentTimeMillis();
         long elapsedTime = end - start;
         System.out.println(elapsedTime);
     }
 
+ //first approach
     public static  String longestPalindrome(String s) {
         int size = s.length();
         if (size == 0 ){
@@ -32,7 +34,6 @@ public class LongestPalindromeSubstring {
 
             for (int i = 0; i < size - range + 1; ++i)
             {
-
                 int j = i + range - 1;
 
                 if (table[i + 1][j - 1] && s.charAt(i) ==
@@ -47,6 +48,49 @@ public class LongestPalindromeSubstring {
             }
         }
         return s.substring(start,start+maxLength);
+    }
+
+    //second approach with Space complexity O(1)
+    public  static String longestPalSubstr(String str) {
+        int maxLength = 1; // The result (length of LPS)
+
+        int start = 0;
+        int len = str.length();
+
+        int low, high;
+
+        // One by one consider every character as center
+        // point of even and length palindromes
+        for (int i = 1; i < len; ++i) {
+            // Find the longest even length palindrome with
+            // center points as i-1 and i.
+            low = i - 1;
+            high = i;
+            while (low >= 0 && high < len
+                    && str.charAt(low) == str.charAt(high)) {
+                if (high - low + 1 > maxLength) {
+                    start = low;
+                    maxLength = high - low + 1;
+                }
+                --low;
+                ++high;
+            }
+
+            // Find the longest odd length palindrome with
+            // center point as i
+            low = i - 1;
+            high = i + 1;
+            while (low >= 0 && high < len
+                    && str.charAt(low) == str.charAt(high)) {
+                if (high - low + 1 > maxLength) {
+                    start = low;
+                    maxLength = high - low + 1;
+                }
+                --low;
+                ++high;
+            }
+        }
+        return str.substring(start, start + maxLength);
     }
 }
 
