dbf9cb07d61864c86841123665a6d10e4da91fce
Morshed Alam <morshedalamustc22@gmail.com>
2020-04-17 02:03:52 +0600

    Reverse Integer problem solved

---
+++ LeetCode/ReverseInteger.java
@@ -0,0 +1,19 @@
+public class ReverseInteger {
+
+    public static void main(String[] args) {
+        System.out.println(reverse(1534236469));
+    }
+    public static int reverse(int x) {
+        long reverse = 0;
+        while (x != 0){
+            int reminder = x%10;
+            reverse =  reverse *10 + reminder;
+            if (reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE){
+                return  0;
+            }
+            x /= 10;
+        }
+        return (int) reverse;
+    }
+
+}
