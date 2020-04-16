d132930c6bf9f37ab5f19cb69f5f38a45d6078de
Morshed Alam <morshedalamustc22@gmail.com>
2020-04-16 23:36:56 +0600

    ZigZagConversion of string problem solved with O(len) time complexity.

---
+++ LeetCode/ZigZagConversion.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class ZigZagConversion {
+    public static void main(String[] args) {
+       String str =   convert("PAYPALISHIRING", 3);
+       System.out.println(str);
+    }
+
+    public static  String convert(String s, int numRows) {
+        if (numRows <= 1){
+            return  s;
+        }
+         char[] charArray = s.toCharArray();
+         String [] strArray = new String[numRows];
+         Arrays.fill(strArray,"");
+         boolean down = true;
+         int row = 0;
+         String newStr  = "";
+
+         for ( int i = 0; i < charArray.length; i++ ){
+             strArray[row] += charArray[i];
+
+             if (row == numRows -1){
+                down = false;
+             }else if (row == 0){
+                 down = true;
+             }
+
+             if (down){
+                 row++;
+             }else{
+                 row--;
+             }
+        }
+     for (int i = 0; i<strArray.length; i++){
+         newStr += strArray[i];
+     }
+    return newStr;
+    }
+}
