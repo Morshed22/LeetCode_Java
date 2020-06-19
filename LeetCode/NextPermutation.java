05a5f1b03b864891fd233d77c3966058818ffe9d
Morshed Alam <morshedalamustc22@gmail.com>
2020-06-20 02:22:29 +0600

    Generate all permutation of an Array and also return all unique permutation has added

--- LeetCode/NextPermutation.java
+++ LeetCode/NextPermutation.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 //https://leetcode.com/articles/next-permutation/
 
 //https://stackoverflow.com/questions/11483060/stdnext-permutation-implementation-explanation
@@ -6,8 +9,9 @@ import java.util.Arrays;
 public class NextPermutation {
     public static void main(String[] args) {
         NextPermutation permutation = new NextPermutation();
-        int [] arr = {1,1,5};
-        permutation.nextPermutation(arr);
+        int [] arr = {1,1,2};
+        //permutation.nextPermutation(arr);
+        System.out.println(permutation.permute(arr));
     }
 
     private void reverse (int nums [], int start){
@@ -38,5 +42,35 @@ public class NextPermutation {
         }
         reverse(nums, i+1);
         //System.out.println(Arrays.toString(nums));
+    }
+    public List<List<Integer>> permute(int[] nums) {
+      List<List<Integer>> list = new ArrayList<>();
+       return permutationOfList(nums,list, 0, nums.length-1);
+
+    }
+
+    private List<List<Integer>> permutationOfList(int[] nums,List<List<Integer>> list, int l, int r){
+        if (l == r){
+            List<Integer> newList = new ArrayList<Integer>();
+            for (int val:nums) newList.add(val);
+            list.add(newList);
+            return list;
+        }
+        for (int i = l; i< nums.length; i++){
+            boolean skip = false;
+            //for remove duplicates this code has used.
+//            for(int j = l; j<i; j++){
+//
+//                if(nums[i] == nums[j]){
+//                    System.out.println(i);
+//                skip  = true;
+//                break;}
+//            }
+//            if (skip) continue;
+            swap(nums, l,i);
+            permutationOfList(nums,list,l+1, r);
+            swap(nums, l,i);
+        }
+        return list;
     }
 }
