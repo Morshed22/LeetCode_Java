e360dfc212b743ff34cbb293200dbd00380b12a9
Morshed Alam <morshedalamustc22@gmail.com>
2020-07-27 00:55:24 +0600

    Wildcard match bettr soln has added

--- LeetCode/WildcardMatching.java
+++ LeetCode/WildcardMatching.java
@@ -1,7 +1,7 @@
 public class WildcardMatching {
     public static void main(String[] args) {
       Solution s = new Solution();
-      System.out.println(s.isMatch("albmnc","a?b*c"));
+      System.out.println(s.comparison("abcdec","ab*c"));
     }
     static class  Solution {
         public boolean isMatch(String s, String p) {
@@ -30,5 +30,43 @@ public class WildcardMatching {
 
             return match[m][n];
         }
+
+
+
+
+
+        //alternative method for this problem
+        boolean comparison(String str, String pattern) {
+            int s = 0, p = 0, match = 0, starIdx = -1;
+            while (s < str.length()){
+                // advancing both pointers
+                if (p < pattern.length()  && (pattern.charAt(p) == '?' || str.charAt(s) == pattern.charAt(p))){
+                    s++;
+                    p++;
+                }
+                // * found, only advancing pattern pointer
+                else if (p < pattern.length() && pattern.charAt(p) == '*'){
+                    starIdx = p;
+                    match = s;
+                    p++;
+                }
+                // last pattern pointer was *, advancing string pointer
+                else if (starIdx != -1){
+                    p = starIdx + 1;
+                    match++;
+                    s = match;
+                }
+                //current pattern pointer is not star, last patter pointer was not *
+                //characters do not match
+                else return false;
+            }
+
+            //check for remaining characters in pattern
+            while (p < pattern.length() && pattern.charAt(p) == '*')
+                p++;
+
+            return p == pattern.length();
+        }
     }
+
 }
