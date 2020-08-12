999b6b1232ee635d676c2300c3233d55c8cd01da
Morshed Alam <morshedalamustc22@gmail.com>
2020-08-12 14:26:08 +0600

    Better method has added for atoi

--- LeetCode/StringToInteger.java
+++ LeetCode/StringToInteger.java
@@ -3,9 +3,33 @@ import javax.swing.*;
 public class StringToInteger {
 
     public static void main(String[] args) {
-        System.out.println(myAtoi("4332"
+        System.out.println(myAtoi2("   -42"
         ));
 
+    }
+    public static  int myAtoi2(String str) {
+        str = str.trim();
+        if (str.length() == 0) return 0;
+        int sign = 1;
+        int result = 0;
+        int i = 0;
+        if (str.charAt(0) == '-' || str.charAt(0) == '+')  {
+            sign = str.charAt(0) == '-' ? -1 : 1;
+            i++;
+        }
+        while (i < str.length()){
+          int num = str.charAt(i) - '0';
+          if (num < 0 || num > 9){
+              break;
+          }
+          //here checking overflow of result.
+          if(Integer.MAX_VALUE/10 < result || (Integer.MAX_VALUE/10 == result && Integer.MAX_VALUE%10< num)){
+              return  sign < 0 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+          }
+          result = result*10+num;
+          i++;
+        }
+       return result*sign;
     }
 
     public static int myAtoi(String str) {
