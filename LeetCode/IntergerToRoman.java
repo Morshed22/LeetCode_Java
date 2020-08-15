22fba69525ed365e590d43f072c05afa8de9ae75
Morshed Alam <morshedalamustc22@gmail.com>
2020-08-15 19:26:50 +0600

    Better runtime method has added

--- LeetCode/IntergerToRoman.java
+++ LeetCode/IntergerToRoman.java
@@ -4,7 +4,7 @@ import java.util.Map;
 public class IntergerToRoman {
 
     public static void main(String[] args) {
-        System.out.println(romanToInt("IX"));
+        System.out.println(romanToInt2("LVIII"));
     }
 
     public static int romanToInt(String s) {
@@ -41,6 +41,45 @@ public class IntergerToRoman {
             value += romanMap.get(String.valueOf(s.charAt(i)));
         }
         return value;
+    }
+    //Symbol       Value
+    //I             1
+    //V             5
+    //X             10
+    //L             50
+    //C             100
+    //D             500
+    //M             1000
+    // better approach for this problem
+    public static int romanToInt2(String s) {
+        int res = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            Character c = s.charAt(i);
+            switch (c) {
+                case 'I':
+                    res += (res >= 5) ? -1 : 1;
+                    break;
+                case 'V':
+                    res += 5;
+                    break;
+                case 'X':
+                    res += 10 * (res >= 50 ? -1 : 1);
+                    break;
+                case 'L':
+                    res += 50;
+                    break;
+                case 'C':
+                    res += 100 * (res >= 500 ? -1 : 1);
+                    break;
+                case 'D':
+                    res += 500;
+                    break;
+                case 'M':
+                    res += 1000;
+                    break;
+            }
+        }
+        return res;
     }
     // better approach for this problem
     public String intToRoman(int num) {
