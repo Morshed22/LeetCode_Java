1a0cc1b0975287ffeca2284ca426644653f83989
Morshed Alam <morshedalamustc22@gmail.com>
2020-08-17 14:57:38 +0600

    4 sum problem new method has added

--- LeetCode/FourSum.java
+++ LeetCode/FourSum.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class FourSum {
     public static void main(String[] args) {
-        System.out.println(fourSum(new int[] {-1,0,1,2,-1,-4},-1));
+        System.out.println(fourSumAgain(new int[] {1,-2,-5,-4,-3,3,3,5},-11));
     }
-//   [-1,0,1,2,-1,-4]
-//           -1
+///[1,-2,-5,-4,-3,3,3,5]
+//        -11
     public static List<List<Integer>> fourSum(int[] nums, int target) {
 
         if (nums.length < 4){
@@ -60,6 +60,39 @@ public class FourSum {
                        low++;
                        high--;
                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    public static List<List<Integer>> fourSumAgain(int[] nums, int target) {
+        if (nums.length < 4) return Collections.emptyList();
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i < nums.length-3; i++){
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            if(nums[i] + nums[i+1]+ nums[i+2] + nums[i+3] > target) break;
+            for(int j = i+1; i < nums.length-2; j++){
+                if (j > i+1 && nums[j] == nums[j-1]) continue;
+                if(nums[i] + nums[j]+ nums[j+1] + nums[j+2] > target) break;
+                int low = j+1;
+                int high = nums.length-1;
+                while (low<high){
+                    int sum =  nums[i] + nums[j]+ nums[low] + nums[high];
+                    if (sum == target){
+                        res.add(Arrays.asList(nums[i] , nums[j], nums[low] , nums[high]));
+                        high--;
+                        low++;
+                        while (low < high  &&  nums[high] == nums[high+1]) high--;
+                        while (low < high  && nums[low] == nums[low-1]) low++;
+                    }else if (sum > target){
+                        high--;
+                        while (low < high  &&  nums[high] == nums[high+1]) high--;
+                    }else {
+                        low++;
+                        while (low < high  && nums[low] == nums[low-1]) low++;
+                    }
                 }
             }
         }
