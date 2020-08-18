8dbdd72177ceaf81df368b7fd752722fb462921f
Morshed Alam <morshedalamustc22@gmail.com>
2020-08-18 22:56:33 +0600

    Reverse Nodes in k-Group has added from leetcode soln

---
+++ LeetCode/ReverseNodeInKGroup.java
@@ -0,0 +1,21 @@
+public class ReverseNodeInKGroup {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode node = head;
+        int count = 0;
+        while ( count < k){
+            if (node == null) return head;
+            node = node.next;
+            count ++;
+        }
+     ListNode pre = reverseKGroup(node, k);
+
+        while (count --> 0){
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
